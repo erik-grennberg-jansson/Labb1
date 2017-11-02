4433990680df56abae4fe850429f7797d01e1891
Erik <erik@Eriks-MacBook-Air.local>
2017-11-02 14:01:00 +0100

    så chad

---
+++ BinSearchGenSet.java
@@ -0,0 +1,8 @@
+public class BinSearchGenSet<E extends Comparable<? super E>> implements GenSet<E>{
+    public void add(E element){
+    }
+    public void remove(E element){}
+    
+    public boolean contains(E element){}
+    
+    
